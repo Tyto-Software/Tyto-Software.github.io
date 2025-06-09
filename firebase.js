b051732dfa6177301ea3d266ecb283427cb45564
PeekoMeeko <62924215+Toonigy@users.noreply.github.com>
2025-06-09 15:11:56 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAck1pPZiPSkXGB_9AnfM4p4-5VZFtlkPE",
+  authDomain: "tytochan-9a2dd.firebaseapp.com",
+  databaseURL: "https://tytochan-9a2dd-default-rtdb.firebaseio.com",
+  projectId: "tytochan-9a2dd",
+  storageBucket: "tytochan-9a2dd.firebasestorage.app",
+  messagingSenderId: "232902880536",
+  appId: "1:232902880536:web:d7b3df3a49a5982974026d",
+  measurementId: "G-JWQGM7KPSH"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
